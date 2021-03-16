32286cc219fdda1e25ba5a3ec87c8601acd43505
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-03-16 11:47:12 +0300

    added GUI layout for ForecastViewController

--- WeatherApp/View/ForecastViewController.swift
+++ WeatherApp/View/ForecastViewController.swift
@@ -7,21 +7,64 @@
 
 import UIKit
 
-import UIKit
-
 class ForecastViewController: UIViewController {
+
+    //MARK: GUI Variables
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Тут должен быть город"
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let colorLine: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "rainbow")
+        view.contentMode = .scaleToFill
+        return view
+    }()
+
+    private let table: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .orange
+        return table
+    }()
 
     var presenter: ForecastViewPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .secondarySystemBackground
-
+        initViews()
     }
 
+    private func initViews() {
+        view.addSubviews([headerLabel, colorLine, table])
+        makeConstraints()
+    }
+
+    private func makeConstraints() {
+        headerLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.left.right.equalToSuperview()
+        }
+
+        colorLine.snp.makeConstraints { make in
+            make.top.equalTo(headerLabel.snp.bottom).offset(10)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(4)
+        }
+
+        table.snp.makeConstraints { make in
+            make.top.equalTo(colorLine.snp.bottom)
+            make.left.right.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 }
 
 extension ForecastViewController: ForecastViewProtocol {
 
 }
+
 
